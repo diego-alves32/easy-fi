eeea8611ec1cafc47ec0800b43dcd31b69d28ff3
diego_alves <diegoalves32@gmail.com>
2024-11-03 16:58:56 -0300

    testeCommit

--- src/routes/incomesExpensesRoutes.js
+++ src/routes/incomesExpensesRoutes.js
@@ -4,6 +4,11 @@ const router = express.Router();
 
 router.post("/cadastroItem", incomesExpensesController.createItem);
 router.get("/buscarItem/:id", incomesExpensesController.getItemById);
-router.get("/buscarExtratoFiltro/:categoria/:dataInicio-:dataFim", incomesExpensesController.getItensByCategoryAndDateRange);
+router.get(
+  "/buscarExtratoFiltro/:categoria/:dataInicio-:dataFim",
+  incomesExpensesController.getItensByCategoryAndDateRange
+);
 router.put("/atualizarItem/:id", incomesExpensesController.updateItem);
 router.delete("/deletarItem/:id", incomesExpensesController.deleteItem);
+
+module.exports = router;
